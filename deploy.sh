12dbb25fe4843dfc7d6c6cc3ba675fc90151a2a8
Kuumernest <100524588+Kuumernest@users.noreply.github.com>
2026-04-16 17:27:24 +0100

    Enhance deploy.sh for Git and Docker installation
    Updated deployment script to install Git and Docker on Ubuntu, start Docker service, and improved echo messages.

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 echo "Checking Git..."
-if ! command -v git &> /dev/null
-then
-    echo "Install Git manually on Windows"
-    exit 1
+if ! command -v git &> /dev/null; then
+    sudo apt update && sudo apt install git -y
 fi
 
 echo "Checking Docker..."
-if ! command -v docker &> /dev/null
-then
-    echo "Install Docker Desktop"
-    exit 1
+if ! command -v docker &> /dev/null; then
+    sudo apt update && sudo apt install docker.io -y
 fi
 
-REPO_URL="https://github.com/YOUR_USERNAME/taskmanager-api.git"
+echo "Starting Docker..."
+sudo systemctl start docker
+sudo systemctl enable docker
 
-cd ~ || exit
+REPO_URL="https://github.com/kuumernest/taskmanager-api.git"
+
+cd ~
 rm -rf taskmanager-api
 
 git clone $REPO_URL
 cd taskmanager-api || exit
 
-echo "Checking files..."
+echo "Checking required files..."
 for file in package.json Dockerfile docker-compose.yml
 do
     if [ ! -f "$file" ]; then
@@ -33,4 +33,4 @@ done
 
 docker compose up --build -d
 
-echo "App running at http://localhost:5500"
+echo "App running at: http://localhost:5500"
